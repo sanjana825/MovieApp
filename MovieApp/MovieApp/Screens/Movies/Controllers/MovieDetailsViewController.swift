f20eb78a3437f7a5960e18ac6c83b568c790a271
sanjana825 <sanjanasb253@gmail.com>
2024-09-17 14:18:09 +0530

    Reorganized folder structure.

--- MovieApp/MovieApp/Screens/Movies/Controllers/MovieDetailsViewController.swift
+++ MovieApp/MovieApp/Screens/Movies/Controllers/MovieDetailsViewController.swift
@@ -93,21 +93,3 @@ extension MovieDetailsViewController{
     }
 }
 
-// MARK: - UIImageView method
-extension UIImageView {
-    func downloaded(from url: URL, contentMode mode: ContentMode = .scaleToFill){
-        contentMode = mode
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            guard
-                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
-                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
-                let data = data, error == nil,
-                let image = UIImage(data: data)
-            else { return }
-            DispatchQueue.main.async() { [weak self] in
-                self?.image = image
-            }
-            
-        }.resume()
-    }
-}
